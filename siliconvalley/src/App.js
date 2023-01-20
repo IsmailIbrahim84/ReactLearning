cab89c36d65d454fec49506f7848659d11237fd8
Ismail Ibrahim <I.Ibrahim84@outlook.com>
2023-01-20 18:13:02 +0200

    Using Map Function to populate records.

--- siliconvalley/src/App.js
+++ siliconvalley/src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {data} from './SpeakerData'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container speakers">
+      <div className="row">
+          {data.map(function (speaker) {
+              const {id, first, last,company, bio,twitterHandle, favorite, sessions} = speaker;
+                return (
+        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
+            <div className="card card-height p-4 mt-4">
+              <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
+                <img className="contain-fit" src={`/images/speaker-${id}.jpg`}
+                width="300"
+                     alt={`${first} ${last}`}/>
+              </div>
+                <div className="speaker-info">
+                    <div className="d-flex justify-content-between mb-3">
+                        <h3 className="text-truncate w-200">
+                            {first} {last}
+                        </h3>
+                    </div>
+                </div>
+                <div>{bio} {company} {twitterHandle} {favorite}</div>
+                <div className="sessionBox card h-250">
+                    <div className="session w-100">
+                    {sessions[0].title} <strong>Room: {sessions[0].room.name}</strong>
+                    </div>
+                    </div>
+            </div>
+        </div>
+
+  )
+          })}
+      </div>
     </div>
-  );
+          );
 }
 
 export default App;
