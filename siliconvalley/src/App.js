dcc7df9a6b673a59fe30ab0251158fd725321ba0
Ismail Ibrahim <I.Ibrahim84@outlook.com>
2023-01-23 15:06:24 +0200

    Adding Session, Sessions and SpeakerImage Component

--- siliconvalley/src/App.js
+++ siliconvalley/src/App.js
@@ -2,6 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import {data} from './SpeakerData'
 
+function Session({session}) {
+    const {title, room} = session;
+    return (
+        <div className="session w-100">
+            {title} <strong>Room: {room.name}</strong>
+        </div>
+    );
+}
+
+
+
+function Sessions({sessions}) {
+    return (
+        <Session session={sessions[0]}/>
+    );
+}
+
+function SpeakerImage({id, first, last}) {
+
+    return (
+        <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
+            <img className="contain-fit" src={`/images/speaker-${id}.jpg`}
+                 width="300"
+                 alt={`${first} ${last}`}/>
+        </div>
+    );
+}
+
 function App() {
 
   return (
@@ -11,13 +39,10 @@ function App() {
           {data.map(function (speaker) {
               const {id, first, last,company, bio,twitterHandle, favorite, sessions} = speaker;
                 return (
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
+        <div key={id} className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
             <div className="card card-height p-4 mt-4">
-              <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
-                <img className="contain-fit" src={`/images/speaker-${id}.jpg`}
-                width="300"
-                     alt={`${first} ${last}`}/>
-              </div>
+                <SpeakerImage id={id} first={first} last={last} />
+
                 <div className="speaker-info">
                     <div className="d-flex justify-content-between mb-3">
                         <h3 className="text-truncate w-200">
@@ -27,9 +52,8 @@ function App() {
                 </div>
                 <div>{bio} {company} {twitterHandle} {favorite}</div>
                 <div className="sessionBox card h-250">
-                    <div className="session w-100">
-                    {sessions[0].title} <strong>Room: {sessions[0].room.name}</strong>
-                    </div>
+                    <Sessions sessions={sessions}/>
+
                     </div>
             </div>
         </div>
