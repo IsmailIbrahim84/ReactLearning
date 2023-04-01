b597adeb6d0979a2464cbef11118f8a87a228845
Ismail Ibrahim <I.Ibrahim84@outlook.com>
2023-04-01 23:13:23 +0200

    Adding validation method and useTheme.js

--- siliconvalley/src/Context/ThemeContext.js
+++ siliconvalley/src/Context/ThemeContext.js
@@ -1,9 +1,10 @@
-import {createContext,useState} from "react";
+import {createContext} from "react";
+import useTheme from "../hooks/useTheme";
 
 export const ThemeContext = createContext();
 
 function ThemeProvider ({startTheme,children}) {
-    const [theme, setTheme] = useState(startTheme);
+    const {theme, setTheme} = useTheme(startTheme);
     return (
         <ThemeContext.Provider value={{theme, setTheme}}>
             <div className={theme === "light" ? "container-fluid light" : "container-fluid dark"}>
