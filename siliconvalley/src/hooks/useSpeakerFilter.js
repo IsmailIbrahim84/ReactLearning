2dc82c2fd052b7d6109411b6dad7e6bc283ba10f
Ismail Ibrahim <I.Ibrahim84@outlook.com>
2023-04-03 03:27:37 +0200

    Adding Speaker Toolbar Dropdown Years and data filtration code

--- siliconvalley/src/hooks/useSpeakerFilter.js
+++ siliconvalley/src/hooks/useSpeakerFilter.js
@@ -5,7 +5,7 @@ function UseSpeakerFilter(startingShowSessions, startingEventYear) {
     const[eventYear, setEventYear] = useState(startingEventYear);
     const [searchQuery, setSearchQuery] = useState("");
 
-    const EVENT_YEARS = ["2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020"];
+    const EVENT_YEARS = ["2005","2006","2007","2008","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020"];
 
     return {showSessions, setShowSessions, eventYear, setEventYear, searchQuery, setSearchQuery, EVENT_YEARS};
 }
